285aa647ccec486c38597860ec72c2ddf038defa
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2022-10-28 21:55:26 +0300

    .

--- Kutuphane-Yonetim-Paneli/Source-Code/Kütüphane Yönetim Otomasyonu/Kütüphane Yönetim Otomasyonu/MainMenu.Designer.cs
+++ Kutuphane-Yonetim-Paneli/Source-Code/Kütüphane Yönetim Otomasyonu/Kütüphane Yönetim Otomasyonu/MainMenu.Designer.cs
@@ -36,9 +36,11 @@
             this.HOME = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -54,25 +56,23 @@
             this.button1.BackColor = System.Drawing.Color.Linen;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(45, 362);
+            this.button1.Location = new System.Drawing.Point(40, 581);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 53);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click_2);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Linen;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button2.Location = new System.Drawing.Point(45, 285);
+            this.button2.Location = new System.Drawing.Point(40, 504);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(110, 51);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // panel2
             // 
@@ -118,12 +118,24 @@
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(40, 237);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(115, 92);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel2);
@@ -137,10 +149,10 @@
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainMenu";
-            this.Load += new System.EventHandler(this.MainMenu_Load);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -154,5 +166,6 @@
         private System.Windows.Forms.Label HOME;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
--- Kutuphane-Yonetim-Paneli/Source-Code/Kütüphane Yönetim Otomasyonu/Kütüphane Yönetim Otomasyonu/MainMenu.cs
+++ Kutuphane-Yonetim-Paneli/Source-Code/Kütüphane Yönetim Otomasyonu/Kütüphane Yönetim Otomasyonu/MainMenu.cs
@@ -17,124 +17,7 @@ namespace Kütüphane_Yönetim_Otomasyonu
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            Members members = new Members();
-            members.ShowDialog();
-            this.Close();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            Books books = new Books();
-            books.ShowDialog();
-            this.Close();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            Employees employees = new Employees();
-            employees.ShowDialog();
-            this.Close();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            Transactions transactions = new Transactions();
-            transactions.ShowDialog();
-            this.Close();
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            Requests requests = new Requests();
-            requests.ShowDialog();
-            this.Close();
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            AboutUs aboutUs = new AboutUs();
-            aboutUs.ShowDialog();
-            this.Close();
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            Statistics statistics = new Statistics();
-            statistics.ShowDialog();
-            this.Close();
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            Categories categories = new Categories();
-            categories.ShowDialog();
-            this.Close();
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            //this.Hide();
-            //Members members = new Members();
-            //members.ShowDialog();
-            //this.Close();
-        }
-
-        private void MainMenu_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            //this.Hide();
-            //Members members = new Members();
-            //members.ShowDialog();
-            //this.Close();
-            //Members form2 = new Members();//açılacak form
-            //form2.MdiParent = this;//bu formu parent olarak veriyoruz.
-            //form2.Show(); //form 2 açılıyor.
-          
-        }
-
-        private void button1_Click_2(object sender, EventArgs e)
-        {
-            //panel1.Controls.Clear(); // Panel'in içini temizliyoruz..
-            //Members frm2 = new Members();
-            //frm2.TopLevel = false;
-            //panel1.Controls.Add(frm2); // panel1 içerisinde formu ekledik
-
-            //frm2.Show(); // formu gösterdik. Ancak buraya dikakt. ShowDialog(); olarak değil Show(); olarak açıyoruz.
-            //frm2.Dock = DockStyle.Fill; // Açılan formun paneli doldurmasını sağladık.
-            //frm2.BringToFront(); // formu panel içinde en öne getirdik
-        }
-
-        private void button2_Click_1(object sender, EventArgs e)
-        {
-            panel1.Controls.Clear(); // Panel'in içini temizliyoruz..
-            Employees frm2 = new Employees();
-            frm2.TopLevel = false;
-            panel1.Controls.Add(frm2); // panel1 içerisinde formu ekledik
-
-            frm2.Show(); // formu gösterdik. Ancak buraya dikakt. ShowDialog(); olarak değil Show(); olarak açıyoruz.
-            frm2.Dock = DockStyle.Fill; // Açılan formun paneli doldurmasını sağladık.
-            frm2.BringToFront(); // formu panel içinde en öne getirdik
-        }
-
+      
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             panel1.Controls.Clear(); // Panel'in içini temizliyoruz..
@@ -151,6 +34,18 @@ namespace Kütüphane_Yönetim_Otomasyonu
         {
             panel1.Controls.Clear(); // Panel'in içini temizliyoruz..
             Employees frm2 = new Employees();
+            frm2.TopLevel = false;
+            panel1.Controls.Add(frm2); // panel1 içerisinde formu ekledik
+
+            frm2.Show(); // formu gösterdik. Ancak buraya dikakt. ShowDialog(); olarak değil Show(); olarak açıyoruz.
+            frm2.Dock = DockStyle.Fill; // Açılan formun paneli doldurmasını sağladık.
+            frm2.BringToFront(); // formu panel içinde en öne getirdik
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear(); // Panel'in içini temizliyoruz..
+            Authors frm2 = new Authors();
             frm2.TopLevel = false;
             panel1.Controls.Add(frm2); // panel1 içerisinde formu ekledik
 
